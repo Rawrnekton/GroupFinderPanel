024241949bd5e213d4b57a3df9d580dc5edc3a31
maknahr <jonathangerdes1@aol.de>
2017-07-01 22:38:30 +0200

    a good night commit

--- src/client/GFPClient.java
+++ src/client/GFPClient.java
@@ -1,28 +1,99 @@
 package client;
 
+import java.util.LinkedList;
+
+import client.model.ClientModel;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class GFPClient extends Application implements EventHandler<ActionEvent>{
+public class GFPClient extends Application{
 
-	@Override
-	public void handle(ActionEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
+	LinkedList<Button> btnList;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		primaryStage.setTitle("GroupFinderPanel");
+		
+		Button btn1 = new Button();
+		btn1.setText("Refresh");
+		btn1.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				//System.out.println("Hello World!");
+				
+				try {
+					@SuppressWarnings("unused")
+					ClientModel test = new ClientModel("FunkyTown");
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				//setBtn("Funky");
+			}
+		});
+		//btnList.add(btn1);
+		
+		int r = 30;
+		Button btn2 = new Button();
+		//btn2.setShape(new Circle(r));
+		btn2.setText("New Game");
+		/*btn2.setMinSize(2 * r, 2 * r);
+		btn2.setMaxSize(2 * r, 2 * r);
+		btn2.setOnAction(new EventHandler<ActionEvent>() {
+			int index = 3;
+			int radius = 30;
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					btnList.add(new Button());
+					btnList.get(index).setShape(new Circle(radius));
+					btnList.get(index).setText(String.valueOf(index));
+					btnList.get(index).setMinSize(2 * radius, 2 * radius);
+					btnList.get(index).setMaxSize(2 * radius, 2 * radius);
+					index++;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		})*/;
+		//btnList.add(btn2);
+		
+
+		System.out.println("did it crash?");
+		
+		BorderPane borderPane = new BorderPane();
+				
+		borderPane.setTop(btn1);
+		borderPane.setTop(btn2);
+		
+		
+		
+		
+		Scene scene = new Scene(borderPane, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 		
 	}
 
-	
-	
 	public static void main(String[] args) {
-		String test = "test";
-		//lol?
+		launch(args);
+	}
+	
+	public void setBtn(String _text) {
+		/*try {
+			btnList.get(0).setText("Funky");
+		} catch (Exception e) {
+			/*
+			 * The only thing necessary for the triumph of evil is for good men to do nothing.
+			 * Just like this bit here.
+			 *
+		}*/
 	}
 }
---
+++ src/client/model/ClientHandler.java
@@ -0,0 +1,100 @@
+package client.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.Callable;
+
+/*
+ * callable Clienthandler der für jeden neuen Client erzeugt wird
+ * und in einem eigene thread dafür sorgt dass der Client seine
+ * Nachricht schickt
+ */
+class ClientHandler implements Callable<String> {
+	/*
+	 * localhost addresse für
+	 */
+	String ip = "127.0.0.1";
+	/*
+	 * port auf dem der client den server sucht
+	 */
+	int port = 50000;
+	/*
+	 * stadtname
+	 */
+	String stadtName;
+	
+	/*
+	 * Konstruktor des Handlers
+	 */
+	public ClientHandler(String stadtName) {
+		this.stadtName = stadtName;
+	}
+	
+	/*
+	 * call() Funktion des interfaces Callable<String>
+	 * ruft die Funktion zum ausgeben der serverantwort auf
+	 * und gibt diese zurück
+	 */
+	@Override
+	public String call() throws Exception {
+		System.out.println("ClientHandler wurde gestartet.");
+		//Thread.sleep(2000);
+		return RequestServer(stadtName);
+		//Thread.sleep(1000);
+	}
+	
+	/*
+	 * Funktion zum Senden und Empfangen der Informationen
+	 */
+	String RequestServer(String _string) throws IOException {
+		String empfangeneNachricht;
+		String zuSendendeNachricht;
+		
+		/*
+		 * Erstellt einen neuen Socket an der angegeben IP, also hier 127.0.0.1:50000
+		 * dies ist die addresse des servers
+		 */
+		Socket socket = new Socket(ip,port);
+		zuSendendeNachricht = _string;
+	    /*
+	     * Sendet den Stadtnamen an den Server
+	     */
+	    schreibeNachricht(socket, zuSendendeNachricht);
+	    /*
+	     * Empfängt die vom Server gesendete Nachricht
+	     */
+	    empfangeneNachricht = leseNachricht(socket);
+	    /*
+	     * schließt den Socket
+	     */
+	    socket.close();
+	    return empfangeneNachricht;
+	  }
+
+	/*
+	 * sendet die Nachricht an den Server, dazu wird ein printwriter erst mit dem outputstream des socket verbunden,
+	 * danach beschrieben und danach geflusht, der Server empfängt auf der anderen Seite diese Nachricht
+	 */
+	void schreibeNachricht(Socket socket, String nachricht) throws IOException {
+		PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+	    printWriter.print(nachricht);
+	    printWriter.flush();
+	}
+	
+	/*
+	 * empfängt die Nachricht vom Server, dazu wird der Funktion die Adresse des socket gegeben
+	 * danach wird ein reader erzeugt der am inputstream des socket "ließt", dieser empfängt die vom Server gesendete Nachricht
+	 * Antwortlänge ist länger als Nachrichtenlänge, da die Antwort wesentlich länger ist.
+	 */
+	String leseNachricht(Socket socket) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		char[] buffer = new char[100];
+		int anzahlZeichen = bufferedReader.read(buffer, 0, 100);
+		String nachricht = new String(buffer, 0, anzahlZeichen);
+		return nachricht;
+	}
+}
