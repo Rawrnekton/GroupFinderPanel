6f6827b42be1460e4426a0624fe681c375e1ffd2
Rawrnekton <jonathangerdes1@aol.de>
2017-07-01 11:54:50 +0200

    test with deleted file

---
+++ src/client/GFPClient.java
@@ -0,0 +1,27 @@
+package client;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+
+public class GFPClient extends Application implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	public static void main(String args[]) {
+		
+	}
+}
