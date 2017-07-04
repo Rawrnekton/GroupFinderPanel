a0190893e7d6b3c76939cb9e0267bc867b4365f7
maknahr <jonathangerdes1@aol.de>
2017-07-04 14:41:48 +0200

    oh okay?

--- src/client/model/DataPackage.java
+++ src/client/model/DataPackage.java
@@ -5,35 +5,56 @@ import java.util.LinkedList;
 
 public class DataPackage {
 
-	private String playerName;
-	private LinkedList<String> games;
+	private String userName;
+	private LinkedList<String> gameList;
 	
 	private int timeUntilReadySeconds;
 	private Date lastManualRefresh;
 	private Date timeUntilExpired;
 	
-	private String currentPlayedGame;
+	private String currentlyPlayedGame;
 	private int inGameSinceSeconds;
 
-	
+	/*
+	 * will use this if loading payload in tiny bits is prefered
+	 */
 	public DataPackage() {
 		this.lastManualRefresh = new Date();
+	}
+	
+	/*
+	 * will use this if everything is already there and ready for takoff
+	 */
+	public DataPackage(String userName, LinkedList<String> gameList, 
+			int timeUntilReadySeconds, Date lastManualRefresh, Date timeUntilExpired,
+			String currentlyPlayedGame, int inGameSinceSeconds) {
+		this.userName = userName;
+		this.gameList = gameList; 
+		this.timeUntilReadySeconds = timeUntilReadySeconds;
+		this.lastManualRefresh = lastManualRefresh;
+		this.timeUntilExpired = timeUntilExpired;
+		this.currentlyPlayedGame = currentlyPlayedGame;
+		this.inGameSinceSeconds = inGameSinceSeconds;
 	}
 	
 	
 	/*
 	 * Set and Add Methods
 	 */
-	public void setPlayerName(String playerName) {
-		this.playerName = playerName;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
 	public void setTimeUntilReady(int timeUntilReadySeconds) {
 		this.timeUntilReadySeconds = timeUntilReadySeconds;
 	}
-
+	
+	public void setGameList(LinkedList<String> gameList) {
+		this.gameList = gameList;
+	}
+	
 	public void addGame(String newGame) {
-		games.add(newGame);
+		gameList.add(newGame);
 	}
 	
 	public void updateDate() {
@@ -42,7 +63,5 @@ public class DataPackage {
 	
 	/*
 	 * Get-Methoden
-	 */
-	
-	
+	 */	
 }
